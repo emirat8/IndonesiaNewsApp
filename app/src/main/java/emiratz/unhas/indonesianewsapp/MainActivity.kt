d3d85c061c1e64b5dc3243b1c04b96b9a969e156
emirat8 <emirat.millenium@yahoo.co.id>
2021-12-12 01:05:30 +0800

    Add navigation component

--- app/src/main/java/emiratz/unhas/indonesianewsapp/MainActivity.kt
+++ app/src/main/java/emiratz/unhas/indonesianewsapp/MainActivity.kt
@@ -2,10 +2,26 @@ package emiratz.unhas.indonesianewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import emiratz.unhas.indonesianewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
